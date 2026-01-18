732716d86f6a73edb8e78208c72ffc36a747df73
Adriel Bigvai Andrade Borrayo <cubos2769.andrade@gmail.com>
2026-01-18 14:52:12 -0600

    prjects doc 40%

--- src/assets/mainComponents/projects.jsx
+++ src/assets/mainComponents/projects.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react'
 import './mainPage.css'
 
 export const Projects = () => {
+  const [projects, setProjects] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/Cubos27/repos?sort=updated&direction=desc`
+        )
+
+        if (!response.ok) throw new Error('Could not fetch repos')
+
+        const data = await response.json()
+        setProjects(data)
+        setLoading(false)
+      } catch (error) {
+
+      }
+
+
+    }
+  }, [])
+
+
   return (
     <div className='container'>
       <h2>My Projects</h2>
